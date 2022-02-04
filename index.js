a2944fa08874c6da8f6225f2ebd0696fd1d8bfee
tyJang <wxd8339@gmail.com>
2022-02-04 23:33:31 +0900

    3

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express()
 const port = 5100
 const { User } = require("./models/User");
 const config = require('./config/key');
+const cookieParser = require('cookie-parser')
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
         mongoose.connect(config.mongoURI, {
@@ -16,12 +18,13 @@ const mongoose = require('mongoose')
         
 app.get('/', (req, res) => res.send('hello world223123'))
 
+
+
 app.post('/register', (req, res) =>{
     //회원가입시 입력했던 정보들을 클라이언트에서 가져오면
     //그것들을 Db에 넣어준다
     const user = new User(req.body)
     //req.body에 {id, password 등 정보갸 담겨져있다} body-parser때문에 사용가능
-
     user.save((err, userInfo) => {
         // save 는 mongodb method이고 req.body에 저장된 정보를 저장 
         if(err) return res.json({ success: false, err})
@@ -30,6 +33,50 @@ app.post('/register', (req, res) =>{
         })
     })
     
+})
+
+
+app.post('/login', (req, res) =>{
+
+    //요청된 email을 데이터베이스에 있는지 찾는다,
+
+    User.findOne({ email: req.body.email}, (err, userInfo) => {
+        //findOne 은 mongoDB 제공 함수, 여러 DB중 (회원가입된) email이 입력받은 email이 있따면
+        //그 줄의 정보를 userInfo로 가져온다
+        if(!userInfo) {
+            return res.json({
+                loginSuccess : false,
+                message: "이메일이 틀렸습니다."
+            })
+        }
+
+     //요청한 email이 있다면 비밀번호도 확인한다.     
+        
+        userInfo.comparePassword(req.body.password, (err, isMatch)=>{
+
+            //findOne으로 찾은 userInfo중 id, password 등 은 req.body.로 찾을수잇다
+            if(!isMatch)
+            return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다"})
+            
+             //비밀번호까지 맞다면 토큰을 생성한다.
+            userInfo.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                
+                //token을 쿠키에 저장
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id})
+            })
+
+        })
+            
+    })
+
+   
+
+   
+
+
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name : {
         type: String,
-        maxlength : 20
+        maxlength : 50
     },
 
     email : {
@@ -13,7 +16,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50
+        maxlength: 60
     },
     role: {
         type: Number,
@@ -25,9 +28,70 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number,
-    },
+    }
 })
 
+userSchema.pre('save', function(next){
+    let user = this; // 암호화되지않은 유저의 비밀번호가 필요하여
+                     // user는 유저스키마를 나타낸다
+    
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+
+                if(err) return next(err);
+                user.password = hash
+                next() 
+            })
+        })
+    }
+    else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        let user = this;
+    console.log('a', user.password, isMatch)
+
+        if(err) return cb(err);
+        cb(null, isMatch);  
+    })
+}
+    
+userSchema.methods.generateToken = function(cb){
+
+    let user = this;
+
+
+    // jsonwebtoken을 이용해서 token생성
+
+    let token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+    //npm example
+    // let jwt = require('jsonwebtoken);
+    // let token = jwt.sign ({foo : 'bar'}, 'shhhh');
+
+}
+    //npm bcrypt example ----------
+    /*bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+            // Store hash in your password DB.
+        });
+    });
+
+    next()
+})
+    */
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User }
